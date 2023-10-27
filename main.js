8bfacdef903f3c9f7e007742882d8477ea1d039c
Ra0pl <147933083+Ra0pl@users.noreply.github.com>
2023-10-27 23:36:25 +0300

    добавил файлы на гит-хаб

---
+++ main.js
@@ -0,0 +1,45 @@
+let changeImgAzov = document.getElementById('azovName');
+let changeImgRostov = document.getElementById('rostovName');
+let changeImgBataisk= document.getElementById('bataiskName');
+let changeImgTaganrog= document.getElementById('TaganrogName');
+let changeImgNovocherkassk= document.getElementById('NovocherkasskName');
+let changeImgShahtu= document.getElementById('ShahtuName');
+let changeImgSalsk= document.getElementById('SalskName');
+let changeImgVolgodonsk= document.getElementById('VolgodonskName');
+let changeImgKamenskShahtinsk= document.getElementById('KamenskShahtinskName');
+
+changeImgAzov.addEventListener('click', ()=>{
+    document.body.style.backgroundImage = "url(images/azovImage.jpg)";
+})
+
+changeImgRostov.addEventListener('click', ()=>{
+    document.body.style.backgroundImage = "url(https://upload.wikimedia.org/wikipedia/commons/6/6f/Rostov_City_Hall_2021.jpg?uselang=ru)";
+})
+
+changeImgBataisk.addEventListener('click', ()=>{
+    document.body.style.backgroundImage = "url(https://upload.wikimedia.org/wikipedia/commons/8/82/%D0%94%D0%9A_%D0%B8%D0%BC._%D0%9B%D0%B5%D0%BD%D0%B8%D0%BD%D0%B0_%D0%B8_%D0%B0%D0%B4%D0%BC%D0%B8%D0%BD%D0%B8%D1%81%D1%82%D1%80%D0%B0%D1%86%D0%B8%D1%8F._%D0%9F%D0%B0%D1%80%D0%BA%2C_%D1%86%D0%B5%D0%BD%D1%82%D1%80._-_panoramio.jpg?uselang=ru)";
+})
+
+changeImgTaganrog.addEventListener('click', ()=>{
+    document.body.style.backgroundImage = "url(https://upload.wikimedia.org/wikipedia/commons/6/60/Alferaki_Palace_2008.jpg?uselang=ru)";
+})
+
+changeImgNovocherkassk.addEventListener('click', ()=>{
+    document.body.style.backgroundImage = "url(https://upload.wikimedia.org/wikipedia/commons/4/43/Triumphal_arch_Novocherkassk.jpg?uselang=ru)";
+})
+
+changeImgShahtu.addEventListener('click', ()=>{
+    document.body.style.backgroundImage = "url(https://upload.wikimedia.org/wikipedia/commons/3/32/%D0%9F%D0%BE%D0%BA%D1%80%D0%BE%D0%B2%D1%81%D0%BA%D0%B8%D0%B9_%D1%81%D0%BE%D0%B1%D0%BE%D1%80_%D0%B3_%D0%A8%D0%B0%D1%85%D1%82%D1%8B.jpg)";
+})
+
+changeImgSalsk.addEventListener('click', ()=>{
+    document.body.style.backgroundImage = "url(https://upload.wikimedia.org/wikipedia/commons/d/d6/Salsk_Rostov_on_Don_region_central_srteet.jpg?uselang=ru)";
+})
+
+changeImgVolgodonsk.addEventListener('click', ()=>{
+    document.body.style.backgroundImage = "url(https://upload.wikimedia.org/wikipedia/ru/2/2f/%D0%9C%D0%B8%D1%80%D0%BD%D1%8B%D0%B9_%D0%B0%D1%82%D0%BE%D0%BC.JPG)";
+})
+
+changeImgKamenskShahtinsk.addEventListener('click', ()=>{
+    document.body.style.backgroundImage = "url(https://upload.wikimedia.org/wikipedia/commons/3/3a/%D0%9A%D0%B0%D0%BC%D0%B5%D0%BD%D1%81%D0%BA_-_%D0%BF%D0%B0%D1%80%D0%BA_%D0%9F%D0%B0%D1%82%D1%80%D0%B8%D0%BE%D1%82_19-1.jpg?uselang=ru)";
+})
